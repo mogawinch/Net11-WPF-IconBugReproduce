23e44518adac857c2165889dfa35ced79f36d567
Marc-André Montreuil <marc-andre.montreuil@ubisoft.com>
2025-12-09 07:37:28 -0500

    Simple app to reproduce bug

---
+++ TestWindow/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace TestWindow
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Simulate heavy startup
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+
+            // Create and show the main window after the delay
+            var mainWindow = new Window();
+            mainWindow.Show();
+        }
+
+    }
+}
---
+++ TestWindow/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TestWindow
+{
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> Items { get; } = new();
+
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set { _isLoading = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            _ = LoadAsync();
+        }
+
+        private async Task LoadAsync()
+        {
+            IsLoading = true;
+            await Task.Delay(10000); // Simulate long load
+            for (int i = 1; i <= 100; i++)
+                Items.Add($"Item {i}");
+            IsLoading = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+}
